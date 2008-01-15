7c2747c190ccf42323377c01029a21609f8ef215
adamc <adamc@3e7fb995-e2d4-4eb6-af63-1e7b6d2af645>
2008-01-15 15:00:28 +0000

    Added logging capability to net-iom-mon.pl.
    git-svn-id: http://svn/office-automation/net-iom/trunk@76 3e7fb995-e2d4-4eb6-af63-1e7b6d2af645

--- net-iom-mon.pl
+++ net-iom-mon.pl
@@ -4,8 +4,17 @@ use strict;
 use warnings;
 
 use NETIOM;
+use Getopt::Long;
 use Term::ReadKey;
-ReadMode 4; # Turn off controls keys
+
+my $logging = 0;
+
+GetOptions ("log" => \$logging) or die;
+
+ReadMode('raw'); # Turn off controls keys
+END {
+    ReadMode('restore'); # Reset tty mode before exiting
+}
 
 my @unit;
 foreach my $unit ('netiom01', 'netiom02') {
@@ -15,6 +24,9 @@ foreach my $unit ('netiom01', 'netiom02') {
 system 'clear';
 
 my $unit_index = 0;
+
+my $state_log;
+
 while ( 1 ) {
 
     # my $key;
@@ -31,7 +43,7 @@ while ( 1 ) {
                 $key = ord(uc($key))-54
             }
             
-            $unit[$unit_index]->set_input_bit($key,$unit[$unit_index]->get_input_bit($key) ? 0 : 1);
+            $unit[$unit_index]->set_input_bit($key, $unit[$unit_index]->get_input_bit($key) ? 0 : 1);
         }
         elsif ( $key =~ m/u/i ) {
             $unit_index = ($unit_index) ? 0 : 1;
@@ -41,7 +53,20 @@ while ( 1 ) {
     my $out;
     
     foreach my $unit (@unit) {
+        
+        my $prev_input_state = $unit->get_unit_name() . '    ' . (join '  ', $unit->get_input());
         $unit->update_state();
+        my $curr_input_state = $unit->get_unit_name() . '    ' . (join '  ', $unit->get_input());
+        
+        if ( ! $unit->{logged} ) {
+            
+            $state_log = $prev_input_state . ' ' . localtime(time())  . "\n" . $state_log;
+            $unit->{logged} = 1;
+        }
+        
+        if ( $prev_input_state ne $curr_input_state ) {
+            $state_log = $curr_input_state . ' ' . localtime(time())  . "\n" . $state_log;
+        }
         
         if ( $unit eq $unit[$unit_index] && $unit[$unit_index]->can('set_input_bit') ) {
             $out .= $unit[$unit_index]->get_unit_name() . " <-- Active unit for input, press u to toggle.\n";
@@ -58,10 +83,7 @@ while ( 1 ) {
     $out .= "Press q to quit.\n";
     system 'clear';
     print $out;
+    print "\nInput log: 01 02 03 04 05 06 07 08 09 10 11 12 13 14 15 16 time\n" if $logging;
+    print $state_log if $logging;
     sleep 1;
 }
-
-END {
-    ReadMode 0; # Reset tty mode before exiting
-}
-
